43a50635f5b418e542fad3984c27de05d08eb509
Sai Rajeswar Mudumba <mudumbas@bart7.iro.umontreal.ca>
2017-06-06 14:47:39 -0400

    first commit

---
+++ text_mlp.py
@@ -0,0 +1,75 @@
+import tensorflow as tf
+import numpy as np
+
+no_features=6
+class Textmlp(object):
+    """
+    A mlp for text classification.
+    Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
+    """
+    def __init__(
+      self, sequence_length, num_classes, vocab_size,
+      embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0):
+
+        # Placeholders for input, output and dropout
+        self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
+        self.input_x1 = tf.placeholder(tf.int32, [None, no_features], name="input_x")
+        self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
+        self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
+
+        # Keeping track of l2 regularization loss (optional)
+        l2_loss = tf.constant(0.0)
+
+        # Embedding layer
+        with tf.device('/cpu:0'), tf.name_scope("embedding"):
+            self.W = tf.Variable(
+                tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
+                name="W")
+            self.embedded_chars = tf.nn.embedding_lookup(self.W, self.input_x)
+            self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
+
+        # Create a convolution + maxpool layer for each filter size
+        pooled_outputs = []
+        h_prev=self.embedded_chars_expanded
+        for i, hidden_size in enumerate(hidden_sizes):
+            with tf.name_scope("mlp-%s" % hidden_size):
+                # Convolution Layer
+
+                W = tf.get_variable(
+                    "W",
+                    shape=[h_prev, hidden_size],
+                    initializer=tf.contrib.layers.xavier_initializer())
+                b = tf.Variable(tf.constant(0.1, shape=[hidden_size]), name="b")
+                out=tf.nn.xw_plus_b(self.h_prev, W, b, name="activations")
+                # Apply nonlinearity
+                h = tf.nn.relu(out, name="relu")
+                # Maxpooling over the outputs
+
+                h_prev=h
+        # Combine all the pooled features
+
+        # Add dropout
+        with tf.name_scope("dropout"):
+            self.h_drop = tf.nn.dropout(self.h, self.dropout_keep_prob)
+
+        # Final (unnormalized) scores and predictions
+        with tf.name_scope("output"):
+            W = tf.get_variable(
+                "W",
+                shape=[num_filters_total, num_classes],
+                initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
+            l2_loss += tf.nn.l2_loss(W)
+            l2_loss += tf.nn.l2_loss(b)
+            self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
+            self.predictions = tf.argmax(self.scores, 1, name="predictions")
+
+        # CalculateMean cross-entropy loss
+        with tf.name_scope("loss"):
+            losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
+            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+
+        # Accuracy
+        with tf.name_scope("accuracy"):
+            correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
